e1e2b286424091d49a417212012ad87aff6d47fb
John Dio Lumacang <tcshs.lumacang.johndio.a@gmail.com>
2026-01-12 22:54:05 +0800

    channel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import './App.css'
+import { useState, useEffect } from "react";
+import { supabase } from "./supbaseClient";
+import "./App.css";
 
 function App() {
+  const [messages, setMessages] = useState([]);
 
-  return (
-    <>
-    </>
-  )
+  useEffect(() => {
+    // fetch initial history (so screen isn't empty)
+    // getMessages()
+
+    // realtime listener for new messages
+    const channel = supabase
+      .channel("public:messages")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "messages" },
+        (payload) => {
+          // LISTEN: when new row is inserted into the DB
+          console.log("New signal received!", payload);
+          const newRow = payload.new;
+
+          // ACTION: add to our state
+          setMessages((prevMessages) => [...prevMessages, newRow]);
+        }
+      );
+  });
+  return <></>;
 }
 
-export default App
+export default App;
